bcc0aa8305adb7754f0092f7218b47607c1158be
Diego Varela <2806@holbertonschool.com>
2021-10-27 17:10:33 -0300

    NavBar WIP

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 new fullpage('#fullpage', {
     autoScrolling : true,
+    navigation: true,
+    onLeave: (origin,destination,direction) =>{
+        const section = destination.item;
+        const title = section.querySelector('h1');
+        const tl = new TimelineMax({delay: 0.5});
+        tl.fromTo(title,0.5, {y: '50', opacity: 0}, {y: 0, opacity: 1});
+    }
 })
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("HeaderNavigation-Items");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
