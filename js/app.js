cded7f203efb44f8794a494497d3c659a760823b
Diego Varela <2806@holbertonschool.com>
2021-10-28 15:54:04 -0300

    The Blobs section ready

--- js/app.js
+++ js/app.js
@@ -2,14 +2,30 @@ new fullpage('#fullpage', {
     autoScrolling : true,
     navigation: true,
     onLeave: (origin,destination,direction) =>{
+        const index = destination.index;
         const section = destination.item;
-        const img = section.querySelector('.section img');
-        const title = section.querySelector('.section .Hello');
-        const text = section.querySelector('.section .text');
         const tl = new TimelineMax({delay: 0.5});
+        switch (index){
+            case 1:
+                const img1 = section.querySelector('.section img');
+                const title = section.querySelector('.section .Hello');
+                const text1 = section.querySelector('.section .text');
+                tl.fromTo(img1,0.5, {x: '50', opacity: 0}, {x: 0, opacity: 1});
+                tl.fromTo(text1,0.5, {x: '-50', opacity: 0}, {x: 0, opacity: 1});
+                break;
+            case 2:
+                const triangle = section.querySelector('.section #triangle');
+                const img2 = section.querySelector('.section #platform');
+                const text2 = section.querySelector('.section .text');
+                tl.fromTo(triangle, 0.5, {x: '50', opacity: 0}, {x: 0, opacity: 1});
+                tl.fromTo(img2, 0.5, {x: '-50', opacity: 0}, {x: 0, opacity: 1});
+                tl.fromTo(text2, 0.5, {y: '-50', opacity: 0}, {y: 0, opacity: 1});
+                break;
+            case 3:
+                break;
+        }
         //tl.fromTo(title,0.5, {y: '50', opacity: 0}, {y: 0, opacity: 1});
-        tl.fromTo(img,0.5, {x: '50', opacity: 0}, {x: 0, opacity: 1});
-        tl.fromTo(text,0.5, {x: '-50', opacity: 0}, {x: 0, opacity: 1});
+        
         //tl.fromTo(title,0.5, {x: '-50', opacity: 0}, {x: 0, opacity: 1});
     }
 })
