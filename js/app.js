81a9bcc31ab8bfd765251581af2a4e954e600198
Diego Varela <2806@holbertonschool.com>
2021-10-27 22:38:46 -0300

    Ronny page with gsap working :D

--- js/app.js
+++ js/app.js
@@ -3,9 +3,14 @@ new fullpage('#fullpage', {
     navigation: true,
     onLeave: (origin,destination,direction) =>{
         const section = destination.item;
-        const title = section.querySelector('h1');
+        const img = section.querySelector('.section img');
+        const title = section.querySelector('.section .Hello');
+        const text = section.querySelector('.section .text');
         const tl = new TimelineMax({delay: 0.5});
-        tl.fromTo(title,0.5, {y: '50', opacity: 0}, {y: 0, opacity: 1});
+        //tl.fromTo(title,0.5, {y: '50', opacity: 0}, {y: 0, opacity: 1});
+        tl.fromTo(img,0.5, {x: '50', opacity: 0}, {x: 0, opacity: 1});
+        tl.fromTo(title,0.5, {x: '-50', opacity: 0}, {x: 0, opacity: 1});
+        tl.fromTo(text,0.5, {x: '-50', opacity: 0}, {x: 0, opacity: 1});
     }
 })
 
